078f4f2ccfd35459eb1d34ba8ce00fe91b24f77d
cally <callyclaver@gmail.com>
2023-03-08 06:11:37 +0100

    added if statement

--- Cally/bash/assignment3.sh
+++ Cally/bash/assignment3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 If statement that compares the first command-line argument with the second compand-line argument
 also check if the first line argument argument is greater
+if [[ $1 -gt $2 ]]
+then
