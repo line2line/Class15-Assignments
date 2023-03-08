b1ced490c95eda95509fc8887f85ed47b17b7f29
Oumar Kal <oumsekal@gmail.com>
2023-03-07 22:52:56 -0600

    stating the condition with if

---
+++ Oumar/filename.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Create an interactive bash script that takes a filename as argument and analyses it.
+#print the permission to the screen, say if it is aregular file or directory.
+
+if [ $# -eq 0 ] 
+then 
