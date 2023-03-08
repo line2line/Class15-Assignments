85df150e8fb8da5bd49c8b95e4306de4ced8a615
Ingrid Dwatie <ingridjeni@yahoo.fr>
2023-03-08 04:47:08 +0000

    Using if statement to print the number

--- Ingrid/evenandodd.sh
+++ Ingrid/evenandodd.sh
@@ -3,6 +3,9 @@
 # Her is the bash script that will print the numbers 1 to 50 and indicate whether they are even or odd.
 
 # Define the range of number
+# Using for loop
 
 for num in {1..50}
+do 
+	if (( num % 2 == 0 ))
 
