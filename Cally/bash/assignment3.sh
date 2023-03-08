c8b387baf2c7ec99531dad7e03bf1423473cee14
cally <callyclaver@gmail.com>
2023-03-08 06:13:17 +0100

    first argument is greater

--- Cally/bash/assignment3.sh
+++ Cally/bash/assignment3.sh
@@ -3,3 +3,5 @@ If statement that compares the first command-line argument with the second compa
 also check if the first line argument argument is greater
 if [[ $1 -gt $2 ]]
 then
+if the first argument is greater, it will print a message indicating that the first argumentis greater than the second argument.
+echo "$1 > $2"
