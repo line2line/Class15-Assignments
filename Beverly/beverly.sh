22fe3e5140a33750d8c88a13dcafadd4413021b8
EC2 Default User <ec2-user@ip-172-31-50-146.ec2.internal>
2023-03-08 17:03:42 +0000

    bash script 1 prompt

---
+++ Beverly/beverly.sh
@@ -0,0 +1,7 @@
+#Bash scripting assignments (3 total)
+
+#Assigment 1 of 3: Create a bash script that takes 2 numbers (that should be taken as CLI arguments: $1 and $2), and prints the larger number on the screen.
+
+#!/bin/bash
+
+
