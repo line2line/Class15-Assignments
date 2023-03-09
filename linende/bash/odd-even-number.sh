042d1508921dabda8bd77fbfb7b01ef1ef5515a2
Line.Nde <lindy2gloire@gmail.com>
2023-03-09 06:14:47 +0000

    added a if statement

--- linende/bash/odd-even-number.sh
+++ linende/bash/odd-even-number.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-for
+for i in {1..50}
 do
+	if [ $(($i%2)) -eq 0 ]
+	then
+	       echo -e "\e[033m $i is an even number\e[00m"
+       else
+	       echo -e "\e[034m $i is an odd number\e[00m"
+	fi
+       
+
+
 done
